2e7df694c4bff338d558ee908c5e0f1ee700d128
Igor Tude <igortude@hotmail.com>
2023-01-17 16:39:31 -0300

    added initial files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+})
+
+$('form').on('submit', function (e){
+    e.preventDefault();
+
+    const inputaddTarefa = $('#aTarefa').val();
+    const novaTarefa = $('<li></li>');
+    //.appendTo()
+
+})
