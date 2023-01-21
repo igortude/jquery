66f637128ea6248ebd35b5d4a93e3733775ffcf7
Igor Tude <igortude@hotmail.com>
2023-01-20 21:32:51 -0300

    Updating main.js & index.html

--- js/main.js
+++ js/main.js
@@ -7,4 +7,9 @@ $('form').on('submit', function (e){
         const novaTarefa = $('<li></li>');
         $(`<li>${adicionarTarefa}</li>`).appendTo(novaTarefa);
         $(novaTarefa).appendTo('ul');
-    })
+        $(adicionarTarefa) = ' ';
+})
+
+$('.teste').click(function (){
+    $('li').css("text-decoration", "line-through");
+})
